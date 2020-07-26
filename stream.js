c1ba120e977a48499ea4bba7dc215a588a651f06
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-25 18:36:37 -0700

    stream

---
+++ stream.js
@@ -0,0 +1,11 @@
+var http = require('http')
+var fs = require('fs')
+
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt')
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received')
+    console.log(chunk)
+    myWriteStream.write(chunk)
+})
