262c7a5bcdb9b86effe613aa6036c93097873b09
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-25 17:37:25 -0700

    third

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var events = require('events')
-var myEmitter = new events.EventEmitter()
+var util = require('util')
 
-myEmitter.on('someEvent', function(mssg){
-    console.log(mssg)
-})
+var Person = function(name){
+    this.name= name
+}
 
-myEmitter.emit('someEvent', 'the event was emitted')
+util.inherits(Person, events.EventEmitter)
 
+var james = new Person('james')
+var mary = new Person('mary')
 // var stuff = require("./stuff")
 
+var people = [james,mary]
+
+people.forEach(function(person){
+    person.on('speak',function(mssg){
+        console.log(person.name + " said:" + mssg)
+    })
+})
+
+james.emit('speak', 'hey dudes')
+mary.emit('speak', 'curry chahiye')
 // console.log(stuff.counter([1,23,4]))
