1523fe5ddde1fcf1a898627cc9c12f6004c26f5d
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-27 15:57:05 -0700

    added more routes

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const PORT = 5000
 const mongoose = require('mongoose')
 const {MONGOURI} = require('./keys')
 
-require('./models/user')
-app.use(express.json())
-
-app.use(require('./routes/auth'))
 
 mongoose.connect(MONGOURI,{
     useUnifiedTopology: true,
@@ -20,6 +16,15 @@ mongoose.connection.on('connected', () =>{
 mongoose.connection.on('error', (err) =>{
     console.log("err", err)
 })
+
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+
 
 app.listen(PORT,()=>{
     console.log("server is running", PORT)
